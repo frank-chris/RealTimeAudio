ae09338309fb1290ce2c0a579d9a54e232aa9903
frank-chris <chrisfrancischris@gmail.com>
2022-02-23 13:16:26 +0530

    Add low pass and FFT, low pass works, FFT has problems

--- record.py
+++ record.py
@@ -18,7 +18,7 @@ python3 record.py [OPTIONS] INDEX
         DURATION is the number of seconds to be recorded. Default: 20
     
     -r, --rate RATE
-        RATE is the sampling rate to be used while recording. Default: 16000
+        RATE is the sampling rate to be used while recording. Default: 1000
 """
 
 import pyaudio
@@ -27,11 +27,15 @@ from matplotlib import pyplot as plt
 from optparse import OptionParser
 from scipy.io.wavfile import write
 from datetime import datetime
+from low_pass import butter_lowpass_filter
 
-CHUNKSIZE = 1024
+CHUNKSIZE = 128
 CHANNELS = 1
+CUTOFF = 3
+ORDER = 5
+FFT_RESOLUTION = 1/6
 
-def record(device_index:int, rate:int = 16000, duration:int = 20, plot:bool = False):
+def record(device_index:int, rate:int = 1000, duration:int = 20, plot:bool = False):
     """
     Function to record and plot microphone audio in real-time
 
@@ -39,7 +43,7 @@ def record(device_index:int, rate:int = 16000, duration:int = 20, plot:bool = Fa
         device_index: int
             index of the input audio device to be used
 
-        rate: int, default: 16000
+        rate: int, default: 1000
             sampling rate to be used while recording
         
         duration: int, default: 20
@@ -56,6 +60,8 @@ def record(device_index:int, rate:int = 16000, duration:int = 20, plot:bool = Fa
         ax = fig.add_subplot(1, 1, 1)
 
         line, = ax.plot([])
+        line_2, = ax.plot([])
+        text = ax.text(0.8, 0.9, "", transform=ax.transAxes)
         ax.set_xlim(0, rate*duration)
         ax.set_ylim(-32800, 32800)
 
@@ -67,18 +73,28 @@ def record(device_index:int, rate:int = 16000, duration:int = 20, plot:bool = Fa
     stream = p.open(format=pyaudio.paInt16, input_device_index=device_index,channels=CHANNELS, rate=rate, input=True, frames_per_buffer=CHUNKSIZE)
 
     frames = []
+    fft_chunk_size = int(rate / CHUNKSIZE / FFT_RESOLUTION)
     for _ in range(int(rate / CHUNKSIZE * duration)):
-        data = stream.read(CHUNKSIZE)
+        data = stream.read(CHUNKSIZE, exception_on_overflow = False)
         frames.append(np.frombuffer(data, dtype=np.int16))
         
+        if _ % fft_chunk_size == 0:
+            amplitude_frame = np.hstack(frames)[-int(rate/ FFT_RESOLUTION):]
+            fft_out = np.fft.fft(amplitude_frame)
+            major_peak = np.argmax(fft_out)*FFT_RESOLUTION
+
         if plot:
             y = np.hstack(frames)
             x = np.linspace(0, len(y), len(y), endpoint=False)
             line.set_data(x, y)
-
+            y_low_pass = butter_lowpass_filter(data=y, cutoff=CUTOFF, fs=rate, order=ORDER)
+            line_2.set_data(x, y_low_pass)
+            text.set_text("Major peak: " + str(major_peak) + " Hz")
             
             fig.canvas.restore_region(background)
             ax.draw_artist(line)
+            ax.draw_artist(line_2)
+            ax.draw_artist(text)
             fig.canvas.blit(ax.bbox)
             fig.canvas.flush_events()
 
@@ -107,7 +123,7 @@ if __name__ == "__main__":
 	help="Number of seconds to be recorded. Default: %default", default=20)
 
     parser.add_option("-r", "--rate", dest="rate", type='int', 
-	help="Sampling rate to be used while recording. Default: %default", default=16000)
+	help="Sampling rate to be used while recording. Default: %default", default=1000)
 
     (options, args) = parser.parse_args()
     
